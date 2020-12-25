0d4c00de300078d5058356775a9e3b7076044829
Stirli <d.v.stanski@outlook.com>
2020-12-25 16:00:29 +0300

    connection closed

--- SomeAspProject/Services/Database/AdoDotNet/AdoDotNetDataService.cs
+++ SomeAspProject/Services/Database/AdoDotNet/AdoDotNetDataService.cs
@@ -23,24 +23,23 @@ namespace SomeAspProject.Services.Database.AdoDotNet
 
         public async Task<DatabaseRequestResult> SendRequestAsync(string request)
         {
-                DatabaseRequestResult result;
+            DatabaseRequestResult result;
             try
             {
-
                 SqlCommand sqlCommand = new SqlCommand(request, _sqlConnection);
                 await _sqlConnection.OpenAsync();
 
-                    var reader = await sqlCommand.ExecuteReaderAsync();
-                    var headers = reader.GetColumnSchema().Select(c => new ColumnDescription { DataType = c.DataType, Name = c.ColumnName }).ToArray();
-                    var rows = new List<object[]>();
-                    while (await reader.ReadAsync())
-                    {
-                        object[] buffer = new object[reader.FieldCount];
-                        reader.GetValues(buffer);
+                var reader = await sqlCommand.ExecuteReaderAsync();
+                var headers = reader.GetColumnSchema().Select(c => new ColumnDescription { DataType = c.DataType, Name = c.ColumnName }).ToArray();
+                var rows = new List<object[]>();
+                while (await reader.ReadAsync())
+                {
+                    object[] buffer = new object[reader.FieldCount];
+                    reader.GetValues(buffer);
 
-                        rows.Add(buffer);
-                    }
-
+                    rows.Add(buffer);
+                }
+                await _sqlConnection.CloseAsync();
                 result = new DatabaseRequestResult()
                 {
                     RecordsAffected = reader.RecordsAffected,
@@ -48,7 +47,7 @@ namespace SomeAspProject.Services.Database.AdoDotNet
                     Rows = rows
                 };
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 result = new DatabaseRequestResult(ex);
             }
